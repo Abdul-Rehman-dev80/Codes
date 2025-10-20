ffb632a7d788a22e3fbd9ead59b70189ccdbde61
Abdul-Rehman-dev80 <abdulrehman.dev80@gmail.com>
2025-10-20 12:41:33 +0500

    solved 2724 sort by

--- Learning/20-Oct.js
+++ Learning/20-Oct.js
@@ -1,5 +1,18 @@
 // 2723 add two promises
 
-function addTwoPromises(promise1, promise2) {
-    return Promise.all([promise1, promise2]).then(value => value[0] + value[1])
+// function addTwoPromises(promise1, promise2) {
+//     return Promise.all([promise1, promise2]).then(value => value[0] + value[1])
+// }
+
+
+
+// 2724 sort by
+
+
+let sortBy = function (arr, fn) {
+    return arr.sort((a,b) => {
+        return fn(a) - fn(b)
+    })
 }
+
+sortBy([[3, 4], [5, 2], [10, 1]], (x) => x[1])
